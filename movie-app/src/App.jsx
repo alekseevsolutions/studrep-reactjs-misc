2290ff12a3cbb3f9b3b6f80bd14f9009ab1327a5
Dmitrii A <alekseevsolutions@gmail.com>
2025-02-01 10:55:31 -0800

    Home component (test)

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 import MovieCard from "./components/MovieCard.jsx"
+import Home from "./pages/Home.jsx";
 
 function App() {
     const  movieNumber = 1;
     return (
       <>
-          {movieNumber === 1 ? (
-              <MovieCard movie={{title: "Dmitrii's Film", release_date: "2024"}}/>
-              ) : (
-              <MovieCard movie={{title: "Joe's Film", release_date: "2020"}}/>)
-          }
+        <Home />
       </>
     )
 }
