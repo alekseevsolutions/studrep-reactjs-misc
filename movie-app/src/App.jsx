28162074f8f8cf6134c5a71b75a13e0aa40e73c5
Dmitrii A <alekseevsolutions@gmail.com>
2025-02-01 11:33:54 -0800

    routing implemented

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import MovieCard from "./components/MovieCard.jsx"
 import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Favorites.jsx";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
     const  movieNumber = 1;
     return (
-      <>
-        <Home />
-      </>
+        <main className={"main-content"}>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+        </main>
     )
 }
 
--- movie-app/src/pages/Favorites.jsx
+++ movie-app/src/pages/Favorites.jsx
@@ -1,6 +1,8 @@
-function Favorite() {
+function Favorites() {
     return <div className="favorites-empy">
         <h2>No Favorite Movies Yet</h2>
         <p>Start adding movies to your favorites and the will appear here</p>
     </div>
 }
+
+export default Favorites
