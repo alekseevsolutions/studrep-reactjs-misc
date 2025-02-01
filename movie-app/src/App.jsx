e4c4cfba72f03d979252897762ffd37e0591f706
Dmitrii A <alekseevsolutions@gmail.com>
2025-02-01 11:43:00 -0800

    navbar component created

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -2,17 +2,21 @@ import './App.css'
 import MovieCard from "./components/MovieCard.jsx"
 import Home from "./pages/Home.jsx";
 import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
 import {Routes, Route} from "react-router-dom"
 
 function App() {
     const  movieNumber = 1;
     return (
-        <main className={"main-content"}>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/favorites" element={<Favorites />} />
-            </Routes>
-        </main>
+        <div>
+            <NavBar />
+            <main className={"main-content"}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+        </div>
     )
 }
 
